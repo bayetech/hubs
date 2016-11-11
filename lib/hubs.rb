acea92c6f56706227dd5e2e790142a485c26f02f
guochunzhong <guochunzhong@bayekeji.com>
2016-11-11 09:27:30 +0800

    Add support of grape

--- lib/hubs.rb
+++ lib/hubs.rb
@@ -5,4 +5,18 @@ module Hub
     engine_name 'hub'
     isolate_namespace Hub
   end
+
+  if defined? Grape
+    cf = File.dirname(__FILE__)
+
+    autoload(:Liker, File.expand_path('../app/models/hubs/liker.rb', cf))
+    autoload(:Notification, File.expand_path('../app/models/hubs/notification.rb', cf))
+    autoload(:LikerNotification, File.expand_path('../app/models/hubs/liker_notification.rb', cf))
+    autoload(:Relationship, File.expand_path('../app/models/hubs/relationship.rb', cf))
+    autoload(:FollowerNotification, File.expand_path('../app/models/hubs/follower_notification.rb', cf))
+    autoload(:Reply, File.expand_path('../app/models/hubs/reply.rb', cf))
+    autoload(:ReplyNotification, File.expand_path('../app/models/hubs/reply_notification.rb', cf))
+    autoload(:Topic, File.expand_path('../app/models/hubs/topic.rb', cf))
+    autoload(:TopicImage, File.expand_path('../app/models/hubs/topic_image.rb', cf))
+  end
 end
