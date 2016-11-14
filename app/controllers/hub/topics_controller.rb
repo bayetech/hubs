98e1c9b022a10355838fb9cb2650f2387a826246
guochunzhong <guochunzhong@bayekeji.com>
2016-11-14 21:35:14 +0800

    Add 1 topic image style.

--- app/controllers/hub/topics_controller.rb
+++ app/controllers/hub/topics_controller.rb
@@ -1,7 +1,7 @@
 module Hub
   class TopicsController < Hub::BaseController
     def index
-      @topics = Topic.page_includes.recent.show_in_recent
+      @topics = Topic.page_includes.recent.show_in_recent.limit 20
     end
   end
 end
