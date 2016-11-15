0e23ac18e5a2164663bc3acd0cf635316a189edc
guochunzhong <guochunzhong@bayekeji.com>
2016-11-15 13:59:34 +0800

    Add counter_cache

--- app/models/hub/topic_image.rb
+++ app/models/hub/topic_image.rb
@@ -1,6 +1,6 @@
 module Hub
   class TopicImage < ActiveRecord::Base
-    belongs_to :topic, foreign_key: :hubs_topic_id
+    belongs_to :topic, foreign_key: :hubs_topic_id, counter_cache: true
 
     mount_uploader :image, Hub::TopicImageUploader
   end
