d84006939eb039c070a32a2b8537911d5fac9af0
guochunzhong <guochunzhong@bayekeji.com>
2016-11-16 22:16:36 +0800

    Show topic

--- app/controllers/hub/topics_controller.rb
+++ app/controllers/hub/topics_controller.rb
@@ -1,9 +1,25 @@
 module Hub
   class TopicsController < Hub::BaseController
     helper Hub::HubHelper
+    before_action :set_topic_and_title, only: [:show]
 
     def index
       @topics = Topic.page_includes.recent.show_in_recent.limit 20
+    end
+
+    def show
+      return redirect_to hubs_topics_path unless @topic.present?
+      @replies = @topic.replies.includes(:customer)
+      @reply = @replies.build
+    end
+
+    private
+
+    def set_topic_and_title
+      @topic = Hub::Topic.find_by(uid: params[:id])
+      return unless @topic.present?
+      @title = @topic.title.truncate(21)
+      @content_title = '巴圈 — 物以类聚，爷以群分'
     end
   end
 end
