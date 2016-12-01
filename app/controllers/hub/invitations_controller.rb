7bd8feba57d432b930c6c11929e6ffe5c32a777e
Eric-Guo <guochunzhong@bayekeji.com>
2016-12-01 15:48:17 +0800

    Add backend logic.

--- app/controllers/hub/invitations_controller.rb
+++ app/controllers/hub/invitations_controller.rb
@@ -1,8 +1,15 @@
 module Hub
   class InvitationsController < ActionController::Base
     layout 'invitation'
+    before_action :set_current_customer
 
     def show
+    end
+
+    private
+
+    def set_current_customer
+      @current_customer = helpers.hub_current_customer
     end
   end
 end
