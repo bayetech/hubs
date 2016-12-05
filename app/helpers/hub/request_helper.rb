36b7bdd461ddb000ff01314d5b7e7d9a09d375f7
Eric-Guo <guochunzhong@bayekeji.com>
2016-12-05 19:50:37 +0800

    New request help moved from WeChat.

---
+++ app/helpers/hub/request_helper.rb
@@ -0,0 +1,35 @@
+module Hub
+  module RequestHelper
+    def wechat_request?
+      request.user_agent.downcase.match(/micromessenger/)
+    rescue
+      false
+    end
+
+    def android_browser_request?
+      ua = request.user_agent.downcase
+      ua.match(/android/) && !ua.start_with?('dalvik')
+    rescue
+      false
+    end
+
+    def android_app_request?
+      request.user_agent.downcase.start_with?('dalvik')
+    rescue
+      false
+    end
+
+    def iphone_browser_request?
+      ua = request.user_agent.downcase
+      ua.match(/iphone|ipad/) && !ua.start_with?('dalvik')
+    rescue
+      false
+    end
+
+    def iphone_ipad_app_request?
+      request.user_agent.downcase.match(/iphone|ipad/)
+    rescue
+      false
+    end
+  end
+end
