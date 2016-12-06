afaa19743023a3143510d322925433ab7e5274c4
Eric-Guo <guochunzhong@bayekeji.com>
2016-12-06 20:24:19 +0800

    Fix ID.

--- app/assets/javascripts/hubs/hub.js
+++ app/assets/javascripts/hubs/hub.js
@@ -36,7 +36,7 @@ function adaptFun(designPercent){
 $(document).ready(function() {
   adaptFun(750/50);
 
-  $('#reply_body').on('focus',function(event){
+  $('#reply-input').on('focus',function(event){
     //自动反弹 输入法高度自适应
     var target = this;
     setTimeout(function(){
